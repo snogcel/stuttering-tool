f3ce67abb5efaee67369624dd94056e4c886b394
snogcel <ledniknoj@gmail.com>
2017-05-18 13:36:06 -0600

    update ui

---
+++ js/interface.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('#generateBtn').on("click", function() {
+
+        // TODO - set up a slider on the page that allows the user to control refresh rate
+        var refreshRate = 3000;
+
+        /*
+        setInterval(function() {
+            $('#generateBtn').trigger('click');
+        }, refreshRate);
+        */
+
+    })
+
+});
