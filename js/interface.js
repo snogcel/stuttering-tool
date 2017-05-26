198b121ee178ae45ca5789dfcf921c798041ee9d
snogcel <ledniknoj@gmail.com>
2017-05-26 09:27:04 -0600

    WIP

--- js/interface.js
+++ js/interface.js
@@ -3,13 +3,41 @@ $(document).ready(function() {
     $('#generateBtn').on("click", function() {
 
         // TODO - set up a slider on the page that allows the user to control refresh rate
+
+
+
+
+
+
+
+        // One step at a time............
+        // 7:45pm rage is a lot easier to handle completely 100%
+
+
+
+
+
+        
+
         var refreshRate = 3000;
 
-        /*
+
         setInterval(function() {
-            $('#generateBtn').trigger('click');
+            $('#generateBtn').trigger('click'); //
         }, refreshRate);
-        */
+
+
+        // TODO - set up a visualizer that shows decibel level to aid students in utilizing tool.
+
+        // Consider using https://www.npmjs.com/package/websockets-streaming-audio
+        // Consider using http://www.storiesinflight.com/jsfft/visualizer/index.html
+        // Consdider using https://wiki.mozilla.org/Audio_Data_API
+        // Use: https://wiki.mozilla.org/Audio_Data_API#Complete_Example:_Creating_a_Web_Based_Tone_Generator
+
+        // Use Buffers because they are extermely complicated.
+
+
+
 
     })
 
